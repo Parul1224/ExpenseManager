8ee336593ebc32dc9e7702560584333d7dbaad7d
“your_username” <agrawal.parul7802@gmail.com>
2019-02-19 17:29:46 +0530

    First Commit

---
+++ AndroidStudioProjects/ExpenseManager/app/src/main/java/com/example/parul/expensemanager/Employee.java
@@ -0,0 +1,13 @@
+package com.example.parul.expensemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
